34159f1bddecd16099f5f1f5d694abfc28b2af18
Creator-Art-Life <maximshool12@gmail.com>
2025-01-03 20:41:50 +0200

    added project
    Added components:
    Navbar
    AnimatedBackground
    Home
    About
    Portfolio

---
+++ src/components/footer.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+type Props = {};
+
+function Footer({}: Props) {
+  return (
+    <footer>
+      <center>
+        <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
+        <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+          © 2025{" "}
+          <a
+            href="https://github.com/Creator-Art-Life"
+            className="hover:underline"
+          >
+            Maxim™
+          </a>
+          . All Rights Reserved.
+        </span>
+      </center>
+    </footer>
+  );
+}
+
+export default Footer;
